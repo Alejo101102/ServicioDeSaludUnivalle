72428697c3da5f4314501256187d92db4b205ca0
Juan Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-12-11 14:07:41 -0500

    Clase Afiliado Creada

---
+++ src/hospital/Afiliado.java
@@ -0,0 +1,29 @@
+package hospital;
+
+/**
+ *    Fundamentos de programación orientada a eventos 750014C-01  
+ *    Laboratorio #
+ *    Profesor: Luis Romo Portilla 
+ *
+ *    Archivo:  Afiliado.java
+ *    Licencia: GNU-GPL 
+ *    @version  1.0
+ *    
+ *    @author   Alejandro Guerrero Cano           (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">}
+ *    @author   Estiven Andres Martinez Granados  (202179687-3743) {@literal <"estiven.martinez@correounivalle.edu.co">}
+ *    @author   Juan David Loaiza Santiago        (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
+ * 
+*/
+
+public class Afiliado extends Persona{
+    
+    /**
+     * Constructor de la clase Afiliado
+     * @see Persona.java - Clase de la que hereda
+     * @param id El numero del documento de identificacion de la persona (int)
+     * @param nombre Los nombre y apelligos de la persona (String)
+     */
+    Afiliado(int id, String nombre){
+        super(id, nombre);
+    }
+}
