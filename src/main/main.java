c8ebc0ba5305b57551c6971f971d108f677ec27b
Alejandro Guerrero Cano <alejogc2002@hotmail.com>
2022-12-13 22:33:52 -0500

    Se crea GUI

---
+++ src/main/main.java
@@ -0,0 +1,29 @@
+package main;
+
+/**
+ *    Fundamentos de programación orientada a eventos 750014C-01  
+ *    Laboratorio # 3
+ *    Profesor: Luis Romo Portilla 
+ *
+ *    Archivo:  main.java
+ *    Licencia: GNU-GPL 
+ *    @version  1.0
+ *    
+ *    @author   Alejandro Guerrero Cano           (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">}
+ *    @author   Estiven Andres Martinez Granados  (202179687-3743) {@literal <"estiven.martinez@correounivalle.edu.co">}
+ *    @author   Juan David Loaiza Santiago        (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
+ * 
+*/
+import vista.*;
+
+public class main {
+    public static void main(String[] args) {
+        VentanaPrincipalVista vpv = new VentanaPrincipalVista();
+        VentanaAfiliadoVista vav = new VentanaAfiliadoVista();
+        VentanaCitasVista vcitv = new VentanaCitasVista();
+        VentanaConsultoriosVista vconv = new VentanaConsultoriosVista();
+        VentanaServiciosVista vsv = new VentanaServiciosVista();
+        
+    }
+    
+}
