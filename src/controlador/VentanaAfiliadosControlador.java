800fe329ba0530a7826e804354a84adfeceedccd
Juan Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-12-14 01:30:25 -0500

    Correccion de comentarios de cabecera

--- src/controlador/VentanaAfiliadosControlador.java
+++ src/controlador/VentanaAfiliadosControlador.java
@@ -1,21 +1,20 @@
 package controlador;
 
 /**
- * Fundamentos de programación orientada a eventos 750014C-01 Laboratorio #
- * Profesor: Luis Romo Portilla
+ *    Fundamentos de programación orientada a eventos 750014C-01  
+ *    Laboratorio # 3
+ *    Profesor: Luis Romo Portilla 
  *
- * Archivo: VentanaAfiliadosControlador.java Licencia: GNU-GPL
- *
- * @version 1.0
- *
- * @author Alejandro Guerrero Cano (202179652-3743)
- * {@literal <"alejandro.cano@correounivalle.edu.co">}
- * @author Estiven Andres Martinez Granados (202179687-3743)
- * {@literal <"estiven.martinez@correounivalle.edu.co">}
- * @author Juan David Loaiza Santiago (202177570-3743)
- * {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
- *
- */
+ *    Archivo:  VentanaAfiliadosControlador.java
+ *    Licencia: GNU-GPL 
+ *    @version  1.1
+ *    
+ *    @author   Alejandro Guerrero Cano           (202179652-3743) {@literal <"alejandro.cano@correounivalle.edu.co">}
+ *    @author   Estiven Andres Martinez Granados  (202179687-3743) {@literal <"estiven.martinez@correounivalle.edu.co">}
+ *    @author   Juan David Loaiza Santiago        (202177570-3743) {@literal <"juan.loaiza.santiago@correounivalle.edu.co">}
+ * 
+*/
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
