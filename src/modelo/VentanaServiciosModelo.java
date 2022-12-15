51023224049d7acaf74cd32fddb831c217b78cef
Juan Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-12-15 12:41:23 -0500

    Creacion e implementacion de métodos (+ Documentación)

--- src/modelo/VentanaServiciosModelo.java
+++ src/modelo/VentanaServiciosModelo.java
@@ -18,11 +18,98 @@ package modelo;
 import hospital.*;
 
 public class VentanaServiciosModelo {  
+    
     BDManager bdmanager = new BDManager();
     
+    private int id;
+    private String nombre;
+    
+    /**
+     * Constructor de la clase VentanaServiciosModelo
+     * @param bdmanager La clase que simula una base de datos
+     */
     public VentanaServiciosModelo(BDManager bdmanager){
         this.bdmanager = bdmanager;
     }
     
-    
+    /**
+     * Obtiene su propio bdmanager
+     *
+     * @return El bdmanager donde tiene todos sus datos
+     */
+    public BDManager getBDManager() {
+        return bdmanager;
+    }
+
+    /**
+     * Obtiene el id de un servicio en especifico
+     *
+     * @param numero La posicion del servicio en el arreglo
+     * @return id El id del servicio (int)
+     */
+    public int getId(int numero) {
+        return bdmanager.getIdServicio(numero);
+    }
+
+    /**
+     * Asigna el id del modelo
+     *
+     * @param id El id de un servicio (int)
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Obtiene el nombre de un servicio específico
+     *
+     * @param numero La posicion del servicio en el arreglo
+     * @return El nombre del servicio(String)
+     */
+    public String getNombre(int numero) {
+        return bdmanager.getNombreServicio(numero);
+    }
+
+    /**
+     * Asigna un nombre al modelo
+     *
+     * @param nombre El nombre de un servicio (String)
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Crea un nuevo servicio en bdmanager usando sus atributos
+     */
+    public void agregarServicio() {
+        bdmanager.addServicio(id, nombre);
+    }
+
+    /**
+     * Modifica un servicio en bdmanager usando sus atributos
+     *
+     * @param idActual El idActual del servicio que se quiere modificar
+     */
+    public void modificarServicio(int idActual) {
+        bdmanager.modServicio(idActual, id, nombre);
+    }
+
+    /**
+     * Elimina un servicio del bdmanager
+     *
+     * @param id El id del servicio que se quiere eliminar
+     */
+    public void eliminarServicio(int id) {
+        bdmanager.delServicio(id);
+    }
+
+    /**
+     * Obtiene la cantidad de servicios que hay
+     *
+     * @return La cantidad de servicios en el arreglo de bdmanager (int)
+     */
+    public int getCantidadServicios() {
+        return bdmanager.getCantidadServicios();
+    }
 }
