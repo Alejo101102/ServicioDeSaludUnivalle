86c478b6f61f0de5c761a359aac0c9e3f10f5581
Juan Loaiza <juan.loaiza.santiago@correounivalle.edu.co>
2022-12-16 14:42:25 -0500

    Correción primera carga de especialistas (No cumplía con el servicio)

--- src/controlador/VentanaCitasControlador.java
+++ src/controlador/VentanaCitasControlador.java
@@ -74,7 +74,6 @@ public class VentanaCitasControlador {
         
         
         cargarServicios();
-        cargarEspecialistas();
         cargarAfiliados();
         
         vista.setGuiaModificar();
